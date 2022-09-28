6de8621aca85df95af31bd5ab450c638a8dfe977
YuryShageev <schageev.yur@yandex.ru>
2022-09-28 13:48:32 +0300

    Примеры по третьему блоку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int age = 19;
+        boolean conNotGoToSchool = age < 7 || age >= 18;
+        if (age >= 18 && age < 21) {
+            System.out.println("Тебе достаточно лет, чтобы водить авто, но не достаточно, чтобы пить алкоголь");
+        }
+        if (conNotGoToSchool) {
+            System.out.println("Я думаю, что ты не ходишь в школу");
+        }
+
+        boolean skyIsClear = true;
+        if (skyIsClear) {
+            System.out.println("Небо чистое");
+        }
     }
 }
